d294e106b24c06f86bac632e720f09905e620d70
Charli7cat <georgy.kyxtanov2003@mail.ru>
2026-01-29 14:52:37 +0300

    OOP

--- ООП. Абстракция и инкапсуляция/Task 1/ООП задание 1.cpp
+++ ООП. Абстракция и инкапсуляция/Task 1/ООП задание 1.cpp
@@ -3,63 +3,113 @@
 #include <vector>
 #include <string>
 
+class Address {
+private:
+    std::string city;
+    std::string street;
+    std::string house;
+    std::string apartment;
+
+public:
+    Address() = default;
+
+    Address(const std::string& city, const std::string& street,
+        const std::string& house, const std::string& apartment)
+        : city(city), street(street), house(house), apartment(apartment) {
+    }
+
+    void readFromStream(std::ifstream& input) {
+        std::getline(input, city);
+        std::getline(input, street);
+        std::getline(input, house);
+        std::getline(input, apartment);
+    }
+
+    void writeToStream(std::ofstream& output) const {
+        output << city << ", " << street << ", " << house << ", " << apartment;
+    }
+
+    std::string getCity() const { return city; }
+    std::string getStreet() const { return street; }
+    std::string getHouse() const { return house; }
+    std::string getApartment() const { return apartment; }
+};
+
+class AddressBook {
+private:
+    std::vector<Address> addresses;
+
+public:
+    void readFromFile(const std::string& filename) {
+        std::ifstream input(filename);
+
+        if (!input.is_open()) {
+            throw std::runtime_error("Couldn't open the file " + filename);
+        }
+
+        int n;
+        input >> n;
+        input.ignore();
+
+        addresses.resize(n);
+
+        for (int i = 0; i < n; ++i) {
+            addresses[i].readFromStream(input);
+        }
+
+        input.close();
+    }
+
+    void writeReversedToFile(const std::string& filename) const {
+        std::ofstream output(filename);
+
+        if (!output.is_open()) {
+            throw std::runtime_error("Couldn't open the file " + filename);
+        }
+
+        output << addresses.size() << std::endl;
+
+        for (int i = addresses.size() - 1; i >= 0; --i) {
+            addresses[i].writeToStream(output);
+            if (i > 0) {
+                output << std::endl;
+            }
+        }
+
+        output.close();
+    }
+
+    size_t getAddressCount() const {
+        return addresses.size();
+    }
+    const Address& getAddress(size_t index) const {
+        if (index >= addresses.size()) {
+            throw std::out_of_range("Индекс выходит за границы массива");
+        }
+        return addresses[index];
+    }
+
+    void addAddress(const Address& address) {
+        addresses.push_back(address);
+    }
+};
 
 int main() {
-    std::ifstream input("in.txt");
-    std::ofstream output("out.txt");
+    try {
+        AddressBook addressBook;
 
+        addressBook.readFromFile("in.txt");
 
-    if (!input.is_open()) {
-        std::cerr << "Error: could not open the file in.txt" << std::endl;
+        addressBook.writeReversedToFile("out.txt");
+
+        std::cout << "The program has been successfully completed!" << std::endl;
+        std::cout << "Addresses processed:" << addressBook.getAddressCount() << std::endl;
+
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error:" << e.what() << std::endl;
         return 1;
     }
-
-    if (!output.is_open()) {
-        std::cerr << "Error: could not open the file out.txt" << std::endl;
-        input.close();
-        return 1;
-    }
-
-    int n;
-    input >> n;
-    input.ignore();
-
-    struct Address {
-        std::string city;
-        std::string street;
-        std::string house;
-        std::string apartment;
-    };
-
-    std::vector<Address> addresses;
-    addresses.reserve(n);
-
-    for (int i = 0; i < n; i++) {
-        Address addr;
-        std::getline(input, addr.city);
-        std::getline(input, addr.street);
-        std::getline(input, addr.house);
-        std::getline(input, addr.apartment);
-        addresses.push_back(addr);
-    }
-
-    output << n << std::endl;
-
-    for (int i = n - 1; i >= 0; --i) {
-        output << addresses[i].city << ", "
-            << addresses[i].street << ", "
-            << addresses[i].house << ", "
-            << addresses[i].apartment;
-
-        if (i > 0) {
-            output << std::endl;
-        }
-    }
-
-    input.close();
-    output.close();
-
-    std::cout << "The program was successfully executed! The result is written to out.txt" << std::endl;
 
     EXIT_SUCCESS;
 }
--- ООП. Абстракция и инкапсуляция/Task 2/OOП задание 2.cpp
+++ ООП. Абстракция и инкапсуляция/Task 2/OOП задание 2.cpp
@@ -4,83 +4,121 @@
 #include <string>
 #include <algorithm>
 
-// Структура для хранения адреса
-struct Address {
+class Address {
+private:
     std::string city;
     std::string street;
     std::string house;
     std::string apartment;
+
+public:
+    Address() = default;
+
+    Address(const std::string& city, const std::string& street,
+        const std::string& house, const std::string& apartment)
+        : city(city), street(street), house(house), apartment(apartment) {
+    }
+
+    void readFromStream(std::ifstream& input) {
+        std::getline(input, city);
+        std::getline(input, street);
+        std::getline(input, house);
+        std::getline(input, apartment);
+    }
+
+    void writeToStream(std::ofstream& output) const {
+        output << city << ", " << street << ", " << house << ", " << apartment;
+    }
+
+    std::string getCity() const { return city; }
+    std::string getStreet() const { return street; }
+    std::string getHouse() const { return house; }
+    std::string getApartment() const { return apartment; }
+
+    static bool compareByCity(const Address& a, const Address& b) {
+        return a.city < b.city;
+    }
 };
 
-// Функция для сравнения двух адресов по названию города
-bool compareByCity(const Address& a, const Address& b) {
-    return a.city < b.city;
-}
+class AddressBook {
+private:
+    std::vector<Address> addresses;
+
+public:
+    void readFromFile(const std::string& filename) {
+        std::ifstream input(filename);
+
+        if (!input.is_open()) {
+            throw std::runtime_error("Couldn't open the file " + filename);
+        }
+
+        int n;
+        input >> n;
+        input.ignore();
+
+        addresses.resize(n);
+
+        for (int i = 0; i < n; ++i) {
+            addresses[i].readFromStream(input);
+        }
+
+        input.close();
+    }
+
+    void sortByCity() {
+        std::sort(addresses.begin(), addresses.end(), Address::compareByCity);
+    }
+
+    void writeToFile(const std::string& filename) const {
+        std::ofstream output(filename);
+
+        if (!output.is_open()) {
+            throw std::runtime_error("Couldn't open the file" + filename);
+        }
+
+        output << addresses.size() << std::endl;
+
+        for (size_t i = 0; i < addresses.size(); ++i) {
+            addresses[i].writeToStream(output);
+            if (i < addresses.size() - 1) {
+                output << std::endl;
+            }
+        }
+
+        output.close();
+    }
+
+    size_t getAddressCount() const {
+        return addresses.size();
+    }
+
+    void addAddress(const Address& address) {
+        addresses.push_back(address);
+    }
+
+    void clear() {
+        addresses.clear();
+    }
+};
 
 int main() {
-    // Открываем файлы для чтения и записи
-    std::ifstream input("in.txt");
-    std::ofstream output("out.txt");
+    try {
+        AddressBook addressBook;
 
-    // Проверяем, открылись ли файлы
-    if (!input.is_open()) {
-        std::cerr << "Error: could not open the file in.txt" << std::endl;
+        addressBook.readFromFile("in.txt");
+
+        addressBook.sortByCity();
+
+        addressBook.writeToFile("out.txt");
+
+        std::cout << "The addresses have been successfully sorted!" << std::endl;
+        std::cout << "Addresses processed: " << addressBook.getAddressCount() << std::endl;
+
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
         return 1;
     }
 
-    if (!output.is_open()) {
-        std::cerr << "Error: Could not open out.txt file" << std::endl;
-        input.close();
-        return 1;
-    }
-
-    
-    int n;
-    input >> n;
-
-    
-    input.ignore();
-
-    
-    std::vector<Address> addresses;
-    addresses.reserve(n);
-
-    
-    for (int i = 0; i < n; ++i) {
-        Address addr;
-
-        std::getline(input, addr.city);
-        std::getline(input, addr.street);
-        std::getline(input, addr.house);
-        std::getline(input, addr.apartment);
-
-        addresses.push_back(addr);
-    }
-
-    
-    std::sort(addresses.begin(), addresses.end(), compareByCity);
-
-    
-    output << n << std::endl;
-
-    
-    for (int i = 0; i < n; ++i) {
-        output << addresses[i].city << ", "
-            << addresses[i].street << ", "
-            << addresses[i].house << ", "
-            << addresses[i].apartment;
-
-        
-        if (i < n - 1) {
-            output << std::endl;
-        }
-    }
-
-    
-    input.close();
-    output.close();
-
-    std::cout << "The addresses were successfully sorted and written to out.txt" << std::endl;
-
-    return 0;
+   EXIT_SUCCESS;
 }
